cd227567913e1a18b1cfdd1cb29882bbda87df57
marcosvalbonet <marcos.valbonet@gmail.com>
2026-04-28 13:38:09 +0200

    Add remote api

---
+++ app/src/main/java/com/example/mymangoapp/di/NetworkModule.kt
@@ -0,0 +1,47 @@
+package com.example.mymangoapp.di
+
+import com.example.mymangoapp.data.remote.api.FakeStoreApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    private const val BASE_URL = "https://fakestoreapi.com/"
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(
+                HttpLoggingInterceptor().apply {
+                    level = HttpLoggingInterceptor.Level.BODY
+                }
+            )
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideFakeStoreApi(retrofit: Retrofit): FakeStoreApi {
+        return retrofit.create(FakeStoreApi::class.java)
+    }
+}
